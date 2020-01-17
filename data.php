6bcf0ee914ab6b5a8910c990783466a55c994951
ZakharSH25 <57192856+ZakharSH25@users.noreply.github.com>
2020-01-17 23:00:29 +0200

    Add files via upload

---
+++ data.php
@@ -0,0 +1,162 @@
+<?php
+
+return [
+    'mainMenu' => [
+        '1' => [
+            'title' => 'Home',
+            'url' => '#',
+        ],
+        '2' => [
+            'title' => 'About',
+            'url' => '#',
+        ],
+        '3' => [
+            'title' => 'Work',
+            'url' => '#',
+        ],
+        '4' => [
+            'title' => 'Services',
+            'url' => '#',
+        ],
+        '5' =>[
+            'title' => 'Blog',
+            'url' => '#',
+        ],
+    ],
+    'siteLogo' => 'assets/img/logo.png',
+    'mainSlider' => [
+        '1' => [
+            'title' => [
+                'main' => 'Alecto',
+                'second' => 'is simple PSD Template',
+            ],
+            'description' => 'Nam varius accumsan elementum aliquam',
+            'link' => [
+                'text' => 'Learn more',
+                'url' => '#',
+            ],
+        ],
+    ],
+    'mainAboutUs' => [
+        '1' => [
+            'title' => 'About us',
+            'description' => 'Nam varius accumsan elementum. Aliquam fermentum eros in suscipit scelerisque.',
+        ],
+            'title' => 'Lorem ipsum',
+            'description' => 'Nam varius accumsan elementum. Aliquam fermentum eros in suscipit scelerisque.',
+    ],
+    'loremIpsum' => [
+            'title' => 'Lorem ipsum',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum augue sed est porttitor, ac
+                        blandit nisl posuere. Pellentesque hendrerit suscipit mi eu lobortis. Curabitur consectetur enim id
+                        elementum accumsan. Quisque metus purus, iaculis id tincidunt non, blandit quis sapien.'
+    ],
+    'dolorSitAmet' => [
+            'title' => 'Dolor sit amet',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum augue sed est porttitor, ac
+                    blandit nisl posuere. Pellentesque hendrerit suscipit mi eu lobortis. Curabitur consectetur enim id
+                    elementum accumsan. Quisque metus purus, iaculis id tincidunt non, blandit quis sapien.',
+    ],
+    'opinion' => [
+            'title' => [
+            'main' => 'Alecto',
+            'second' => '  lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            ],
+
+            'description' => [
+            'main' => 'Adam Czajkowski',
+            'second' => ', CEO&Founder at symu.co'
+            ],
+        'image' => 'assets/img/Adam_Czajkowski.png'
+    ],
+    'services' => [
+        '1' => [
+            'title' => 'Our Services',
+            'description' => 'Nam varius accumsan elementum. Aliquam fermentum eros in suscipit scelerisque.',
+        ],
+        '2' => [
+            'title' => 'Portfolio',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum augue sed est porttitor, ac blandit nisl posuere.',
+        ],
+        '3' => [
+            'title' => 'Design',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum augue sed est porttitor, ac blandit nisl posuere.',
+        ],
+        '4' => [
+            'title' => 'Branding',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum augue sed est porttitor, ac blandit nisl posuere.',
+        ],
+        '5' => [
+            'title' => 'Development',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque elementum augue sed est porttitor, ac blandit nisl posuere.',
+        ],
+    ],
+    'team' => [
+            'title' => 'Meet our team',
+            'description' => ' Nam varius accumsan elementum. Aliquam fermentum eros in suscipit scelerisque.',
+            'name' => 'John Doe',
+    ],
+    'pricing' => [
+        '1' => [
+            'title' => 'Meet our team',
+            'description' => 'Nam varius accumsan elementum. Aliquam fermentum eros in suscipit scelerisque.',
+        ],
+        '2' => [
+            'price' => [
+                'main' => '$2',
+                'second' => '9'
+            ],
+            'title' => 'Lorem ipsum license',
+            'description' => 'Lorem ipsum dolor sit amet
+                        consectetur adipiscing elit
+                        Ante sed interdum labortis
+                        Est nisi efficitur lactus',
+        ],
+        '3' => [
+            'price' => [
+                'main' => '$5',
+                'second' => '9'
+            ],
+            'title' => 'Lorem ipsum license',
+            'description' => 'Lorem ipsum dolor sit amet
+                        consectetur adipiscing elit
+                        Ante sed interdum labortis
+                        Est nisi efficitur lactus',
+        ],
+        '4' => [
+            'price' => [
+                'main' => '$9',
+                'second' => '9'
+            ],
+            'title' => 'Lorem ipsum license',
+            'description' => 'Lorem ipsum dolor sit amet
+                        consectetur adipiscing elit
+                        Ante sed interdum labortis
+                        Est nisi efficitur lactus',
+        ],
+        '5' => [
+            'price' => [
+                'main' => '$12',
+                'second' => '9'
+            ],
+            'title' => 'Lorem ipsum license',
+            'description' => 'Lorem ipsum dolor sit amet
+                        consectetur adipiscing elit
+                        Ante sed interdum labortis
+                        Est nisi efficitur lactus',
+        ],
+        'button' => 'P U R C H A S E',
+    ],
+    'footer' => [
+        'title' => [
+            'main' => 'Try our ',
+            'second' => 'PSD template',
+            'third' => ' on symu.co',
+        ],
+        'button' => 'symu.co/freebies',
+        'footerBot' => [
+            'image' => 'src/img/footer-logo.png',
+            'title' => 'All Rights Reserved by Alecto',
+        ],
+    ],
+];
